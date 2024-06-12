975e122338258d7efec1745d6f9888cdfbe18dcb
sabrina mendonça <sazonass.m@gmail.com>
2024-06-12 11:20:13 -0300

    ajustes finais

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="bg-[#10316b] flex flex-col justify-center items-center shadow-md p-4">
-            <img src={`${process.env.PUBLIC_URL}/img/logo.png`} width={220} alt="logo schoolpass" />
+        <header className="bg-[#10316b] flex flex-col justify-center items-center shadow-md">
+            <img src={`${process.env.PUBLIC_URL}/img/logo.png`} width={200} alt="logo schoolpass" />
         </header>
     )
 }
